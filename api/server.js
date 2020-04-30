a527d10788fac7f300d982b45fe9bb18ec9956ff
tomytank <github@iamtom.com>
2020-04-30 10:50:01 +0200

    Investigating Server get sevices

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ server.use(express.json());
 server.get('/', (req, res) => {
   Shoutouts.find()
   .then(shoutouts => {
-    res.status(200).json(shoutouts);
+    res.status(200).json({shoutouts});
   })
   .catch (error => {
     console.error('\nERROR', error);
