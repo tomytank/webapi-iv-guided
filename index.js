069caa1cd036c911431afed3e8fecbcf0f2c632c
tomytank <github@iamtom.com>
2020-04-30 10:41:46 +0200

    dotenv added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const server = require('./api/server.js');
 
+const motd = process.env.MOTD;
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log('\n*** Server Running on http://localhost:${port} ***\n');
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  console.log(motd);
 });
